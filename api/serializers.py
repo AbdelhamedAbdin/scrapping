b53ee8effdeb5fe683e001c4f811ab2fe2a7f2ee
AbdelhamedAbdin <31192598+AbdelhamedAbdin@users.noreply.github.com>
2022-07-30 01:36:22 +0200

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -4,7 +4,6 @@ from rest_pandas import PandasSerializer
 
 
 class BookSerializer(serializers.Serializer):
-    # download = serializers.SerializerMethodField(read_only=True, label="تحميل الكتاب", default="http://127.0.0.1:8000/5/")
 
     class Meta:
         list_serializer_class = PandasSerializer
